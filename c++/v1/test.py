61b3d9705e61272687e6b5a66b9878bb42add659
wangli <freelw81@qq.com>
2025-03-03 10:23:00 +0800

    test grad is good, bug wip

--- c++/v1/test.py
+++ c++/v1/test.py
@@ -86,6 +86,68 @@ def test1():
     print(model[2].bias)
     print(model[2].bias.grad)
 
+def test2():
+    # 3 layer neural network
+    # input layer: 10 neurons
+    # hidden layer: 20 neurons
+    # output layer: 11 neurons 
+    # activation function: ReLU
+    # loss function: CrossEntropyLoss
+
+    # input from 0 to 9
+    x = [
+        [i+10 for i in range(7)],
+        [10-i for i in range(7)]
+        ]
+    x = torch.tensor(x, dtype=torch.float32).view(2, 7)
+    # target is 1
+    y = [1, 0]
+    y = torch.tensor(y, dtype=torch.long).view(2)
+    # model
+    model = nn.Sequential(
+        nn.Linear(7, 4),
+        nn.ReLU(),
+        nn.Linear(4, 3)
+    )
+    # initialize the weights to 0.1
+    # initialize the bias to 0
+    model[0].weight.data.fill_(0.1)
+    model[0].bias.data.fill_(0.1)
+    model[2].weight.data.fill_(0.1)
+    model[2].bias.data.fill_(0.1)
+
+    model[0].weight.data[0, 0] = 0.9
+    model[0].weight.data[1, 0] = -0.9
+
+    model[2].weight.data[0, 0] = 0.9
+    model[2].weight.data[1, 0] = -0.9
+
+
+    # loss function
+    loss_fn = nn.CrossEntropyLoss()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)  # Change learning rate to 0.001
+    optimizer.zero_grad()
+
+    for e in range(20):
+        res = model(x)
+        # print("forward result: ", res)
+        loss = loss_fn(res, y)
+        print(e, "loss: ", loss)
+        loss.backward()
+        optimizer.step()
+
+        #show the gradients
+
+    print(model[0].weight)
+    print(model[0].weight.grad)
+    print(model[0].bias)
+    print(model[0].bias.grad)
+    print(model[2].weight)
+    print(model[2].weight.grad)
+    print(model[2].bias)
+    print(model[2].bias.grad)
+
 def testce():
 
     # input from 0 to 9
@@ -138,6 +200,6 @@ def testmodule():
 
 if __name__ == '__main__':
 
-    test1()
+    test2()
     #testce()
     #testmodule()
--- c++/v2/testgrad.cpp
+++ c++/v2/testgrad.cpp
@@ -2,9 +2,36 @@
 #include "models.h"
 #include <iostream>
 
+void testgrad_bak() {
+    MLP m(2, {3, 3}, false);
+    m.init();
+
+    auto parameters =  m.get_parameters();
+    (*parameters[0]->get_weight())[0][0] = 0.9;
+    (*parameters[0]->get_weight())[1][0] = -0.9;
+    (*parameters[2]->get_weight())[0][0] = 0.9;
+    (*parameters[2]->get_weight())[1][0] = -0.9;
+    cout << *parameters[0]->get_weight() << endl;
+    cout << *parameters[2]->get_weight() << endl;
+
+    Matrix *input = allocTmpMatrix(Shape(2, 1));
+    (*input)[0][0] = 10;
+    (*input)[1][0] = 11;
+    std::vector<uint> labels;
+    labels.push_back(1);
+
+    m.backward(input, labels);
+    for (auto p : m.get_parameters()) {
+        cout << *p << endl;
+    }
+    freeTmpMatrix();
+}
+
+void optimize(const std::vector<Parameters*> &parameters, DATATYPE lr, int epoch);
+
 void testgrad() {
 
-    MLP m(2, {3, 3}, false);
+    MLP m(7, {4, 3}, false);
     m.init();
 
     auto parameters =  m.get_parameters();
@@ -16,21 +43,23 @@ void testgrad() {
     cout << *parameters[0]->get_weight() << endl;
     cout << *parameters[2]->get_weight() << endl;
 
-
-    Matrix *input = allocTmpMatrix(Shape(2, 1));
-    (*input)[0][0] = 10;
-    (*input)[1][0] = 11;
+    Matrix *input = allocTmpMatrix(Shape(7, 2));
+    for (uint i = 0; i < 7; ++ i) {
+        (*input)[i][0] = 10 + i;
+        (*input)[i][1] = 10 - i;
+    }
+    
     std::vector<uint> labels;
     labels.push_back(1);
-
-
-
-    m.backward(input, labels);
-
+    labels.push_back(0);
+    for (uint k = 0; k < 20; ++ k) {
+        m.zero_grad();
+        auto loss = m.backward(input, labels);
+        cout << k << " loss : " << loss << endl;
+        optimize(m.get_parameters(), 0.001, k);
+    }
     for (auto p : m.get_parameters()) {
         cout << *p << endl;
     }
-    
-
     freeTmpMatrix();
 }
