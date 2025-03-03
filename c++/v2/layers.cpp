60238efce02ae185ed2a84ce8ed566f313a5d9f6
wangli <freelw81@qq.com>
2025-03-03 14:37:06 +0800

    opt

--- c++/v2/layers.cpp
+++ c++/v2/layers.cpp
@@ -131,7 +131,6 @@ Matrix *CrossEntropyLoss::forward(Context * ctx, Matrix *input) {
     ce_ctx->input = input;
     assert(input->getShape().colCnt == labels.size());
     Matrix *mExp = allocTmpMatrix(input);
-    Matrix *ce_for_eachbach = allocTmpMatrix(Shape(1, labels.size()));
     Matrix *loss = allocTmpMatrix(Shape(1,1));
     DATATYPE loss_value = 0;
     for (uint j = 0; j < input->getShape().colCnt; ++ j) {
@@ -155,8 +154,7 @@ Matrix *CrossEntropyLoss::forward(Context * ctx, Matrix *input) {
         p.sum = sum;
         p.max = max;
         ce_ctx->info.push_back(p);
-        (*ce_for_eachbach)[0][j] = -log(ez/sum);
-        loss_value += (*ce_for_eachbach)[0][j];
+        loss_value += -log(ez/sum);
     }
     // assert(ce_ctx->info.size() == labels.size());
     (*loss)[0][0] = loss_value/labels.size();
