ae090c35e5ab117ff885c6c5a0266ddf4ae072b3
wangli <freelw81@qq.com>
2025-03-07 13:42:03 +0800

    update

--- c++/rnn/main.cpp
+++ c++/rnn/main.cpp
@@ -6,6 +6,7 @@
 #include "dataloader.h"
 #include "rnnlm.h"
 #include "optimizers/optimizers.h"
+#include "getopt.h"
 
 // #pragma message("warning: shutdown is true")
 // bool shutdown = true; // fixme
@@ -49,9 +50,12 @@ std::string generateDateTimeSuffix() {
     return oss.str();
 }
 
-void save_checkpoint(RnnLM &lm, std::string filename) {
+void save_checkpoint(RnnLM &lm) {
+    std::cout << "shutting down" << std::endl;
+    std::string checkpoint_name = "checkpoint" + generateDateTimeSuffix() + ".bin";
+    std::string path = "./checkpoints/" + checkpoint_name;
     auto parameters = lm.get_parameters();
-    std::ofstream out(filename, std::ios::out | std::ios::binary);
+    std::ofstream out(path, std::ios::out | std::ios::binary);
     int num_params = parameters.size();
     out.write((char *)&num_params, sizeof(num_params));
     for (auto p : parameters) {
@@ -61,6 +65,7 @@ void save_checkpoint(RnnLM &lm, std::string filename) {
         out.write(serialized.c_str(), serialized.size());
     }
     out.close();
+    cout << "checkpoint saved : " << path << endl;
 }
 
 void loadfrom_checkpoint(RnnLM &lm, std::string filename) {
@@ -78,106 +83,126 @@ void loadfrom_checkpoint(RnnLM &lm, std::string filename) {
     }
 }
 
+void train(const std::string &corpus, const std::string &checkpoint, uint epochs) {
+    std::cout << "train by " << corpus << std::endl;
+    std::cout << "epochs : " << epochs << std::endl;
+    DataLoader loader(corpus);
+    std::cout << "Data loaded" << std::endl;
+    uint num_steps = 32;
+    uint hidden_num = 32;
+    bool rand = true;
+    Rnn *rnn = new Rnn(INPUT_NUM, hidden_num, 0.01, rand);
+    RnnLM lm(rnn, INPUT_NUM, rand);
+    if (!checkpoint.empty()) {
+        cout << "loading from checkpoint : " << checkpoint << endl;
+        loadfrom_checkpoint(lm, checkpoint);
+        cout << "loaded from checkpoint" << endl;
+    }
+    auto parameters = lm.get_parameters();
+    if (!rand) {
+        init_weight(parameters[3]);
+    }
+    Adam adam(parameters, 0.001);
+    for (uint epoch = 0; epoch < epochs; epoch++) {
+        DATATYPE loss_sum = 0;
+        for (uint i = 0; i < loader.data.size() - num_steps; i++) {
+            std::vector<Matrix *> inputs;
+            std::vector<uint> labels;
+            for (uint j = 0; j < num_steps; j++) {
+                assert(i+j < loader.data.size());
+                assert(i+j+1 < loader.labels.size());
+                inputs.push_back(loader.data[i+j]);
+                labels.push_back(loader.labels[i+j+1]);
+            }
+            assert(inputs.size() == num_steps);
+            RnnLMContext *ctx = lm.init();
+            Matrix *res = lm.forward(ctx, inputs);
+            res->checkShape(Shape(INPUT_NUM, num_steps));
+            CrossEntropyLoss loss_fn(labels);
+            CrossEntropyLossContext *ce_ctx = (CrossEntropyLossContext *)loss_fn.init();
+            auto loss = loss_fn.forward(ce_ctx, res);
+            loss->checkShape(Shape(1, 1));
+            loss_sum += (*loss)[0][0];
+            auto grad = loss_fn.backward(ce_ctx, nullptr);
+            loss_fn.release(ce_ctx);
+            lm.zero_grad();
+            lm.backward(ctx, grad);
+            lm.clip_grad(1);
+            adam.step();
+            lm.release(ctx);
+            freeTmpMatrix();
+            if (shutdown) {
+                save_checkpoint(lm);
+                exit(0);
+            }
+        }
+        std::cout << "epoch " << epoch << " loss : " << loss_sum/(loader.data.size() - num_steps) << std::endl;
+    }
+    if (epochs > 0) {
+        save_checkpoint(lm);
+    } else {
+        std::cout << "serving mode" << std::endl;
+    }
+    std::vector<std::string> prefixs = {
+        "time traveller",
+        "the time machine",
+    };
+    for (auto prefix : prefixs) {
+        std::string predicted = lm.predict(prefix, 10);
+        std::cout << "prefix : " << prefix << std::endl;
+        std::cout << "predicted : " << predicted << std::endl;
+    }
+    freeTmpMatrix();
+    delete rnn;
+}
+
 int main(int argc, char *argv[]) {
     // register signal SIGINT and signal handler
     signal(SIGINT, signal_callback_handler);
 
-    bool test = false;
-    bool testdl = false;
-    bool testce = false;
-    bool use_checkpoint = false;
-    if (argc == 2) {
-        if (std::string(argv[1]) == "test") {
-            test = true;
-        } else if (std::string(argv[1]) == "testdl") {
-            testdl = true;
-        } else if (std::string(argv[1]) == "testce") {
-            testce = true;
+    std::string corpus;
+    std::string checkpoint;
+    std::string testcase;
+    int opt;
+    uint epochs = 30;
+    while ((opt = getopt(argc, argv, "f:c:t:e:")) != -1) {
+        switch (opt) {
+            case 'f':
+                corpus = optarg;
+                break;
+            case 'c':
+                checkpoint = optarg;
+                break;
+            case 't':
+                testcase = optarg;
+                break;
+            case 'e':
+                std::cout << "epochs : " << optarg << std::endl;
+                epochs = atoi(optarg);
+                break;
+            default:
+                std::cerr << "Usage: " << argv[0] << " -f <corpus> -c <checpoint> -t <testcase> -e <epochs>" << std::endl;
+                return 1;
         }
     }
 
-    if (argc == 3) {
-        if (std::string(argv[1]) == "load") {
-            use_checkpoint = true; 
+    if (!testcase.empty()) {
+        if (testcase == "test") {
+            testgrad();
+        } else if (testcase == "testdl") {
+            load_data();
+        } else if (testcase == "testce") {
+            testcrossentropy();
+        } else {
+            std::cerr << "not supported testcase" << std::endl;
+            return 1;
         }
     }
 
-    if (test) {
-        testgrad();
-    } else if (testdl) {
-        load_data();
-    } else if (testce) {
-        testcrossentropy();
-    } else {
-        DataLoader loader(RESOURCE_NAME);
-        std::cout << "Data loaded" << std::endl;
-        uint num_steps = 32;
-        uint hidden_num = 32;
-        bool rand = true;
-        Rnn *rnn = new Rnn(INPUT_NUM, hidden_num, 0.01, rand);
-        RnnLM lm(rnn, INPUT_NUM, rand);
-        if (use_checkpoint) {
-            std::string filename = std::string(argv[2]);
-            cout << "loading from checkpoint : " << filename << endl;
-            loadfrom_checkpoint(lm, filename);
-            cout << "loaded from checkpoint" << endl;
-        }
-        auto parameters = lm.get_parameters();
-        if (!rand) {
-            init_weight(parameters[3]);
-        }
-        
-        Adam adam(parameters, 0.001);
-        for (uint epoch = 0; epoch < 30; epoch++) {
-            DATATYPE loss_sum = 0;
-            for (uint i = 0; i < loader.data.size() - num_steps; i++) {
-                std::vector<Matrix *> inputs;
-                std::vector<uint> labels;
-                for (uint j = 0; j < num_steps; j++) {
-                    assert(i+j < loader.data.size());
-                    assert(i+j+1 < loader.labels.size());
-                    inputs.push_back(loader.data[i+j]);
-                    labels.push_back(loader.labels[i+j+1]);
-                }
-                assert(inputs.size() == num_steps);
-                RnnLMContext *ctx = lm.init();
-                Matrix *res = lm.forward(ctx, inputs);
-                res->checkShape(Shape(INPUT_NUM, num_steps));
-                CrossEntropyLoss loss_fn(labels);
-                CrossEntropyLossContext *ce_ctx = (CrossEntropyLossContext *)loss_fn.init();
-                auto loss = loss_fn.forward(ce_ctx, res);
-                loss->checkShape(Shape(1, 1));
-                loss_sum += (*loss)[0][0];
-                auto grad = loss_fn.backward(ce_ctx, nullptr);
-                loss_fn.release(ce_ctx);
-                lm.zero_grad();
-                lm.backward(ctx, grad);
-                lm.clip_grad(1);
-                adam.step();
-                lm.release(ctx);
-                freeTmpMatrix();
-                if (shutdown) {
-                    // exit
-                    std::cout << "shutting down" << std::endl;
-                    std::string checkpoint_name = "checkpoint" + generateDateTimeSuffix() + ".bin";
-                    std::string path = "./checkpoints/" + checkpoint_name;
-                    save_checkpoint(lm, path);
-                    exit(0);
-                }
-            }
-            std::cout << "epoch " << epoch << " loss : " << loss_sum/(loader.data.size() - num_steps) << std::endl;
-        }
-        std::vector<std::string> prefixs = {
-            "time traveller",
-            "the time machine",
-        };
-        for (auto prefix : prefixs) {
-            std::string predicted = lm.predict(prefix, 10);
-            std::cout << "prefix : " << prefix << std::endl;
-            std::cout << "predicted : " << predicted << std::endl;
-        }
-        freeTmpMatrix();
-        delete rnn;
+    if (corpus.empty()) {
+        corpus = RESOURCE_NAME;
     }
+
+    train(corpus, checkpoint, epochs);
     return 0;
 }
