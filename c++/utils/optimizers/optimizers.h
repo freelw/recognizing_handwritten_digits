9517ba8809a5b4b1fc0b0289a4fb1d206cbc353f
wangli <freelw81@qq.com>
2025-03-10 12:24:18 +0800

    adam beta2 对齐pytorch

--- c++/utils/optimizers/optimizers.h
+++ c++/utils/optimizers/optimizers.h
@@ -5,7 +5,7 @@
 class Adam {
     public:
         Adam(std::vector<Parameters*> _parameters, DATATYPE _lr,
-            DATATYPE _beta1 = 0.9, DATATYPE _beta2 = 0.95, DATATYPE _epsilon = 1e-8)
+            DATATYPE _beta1 = 0.9, DATATYPE _beta2 = 0.999, DATATYPE _epsilon = 1e-8)
             : parameters(_parameters), lr(_lr),
                 beta1(_beta1), beta2(_beta2), epsilon(_epsilon)
             {}
