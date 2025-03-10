af5ce2bcc0e26060846022e33ddb8b1451df43aa
wangli <freelw81@qq.com>
2025-03-10 14:33:28 +0800

    fix

--- c++/lstm/test.py
+++ c++/lstm/test.py
@@ -88,8 +88,7 @@ def get_timemachine():
         return f.read()
 
 def tokenize(text):
-    return list(text)[:2000] # fix me
-    #return list(text)
+    return list(text)
 
 def one_hot(x, vocab_size):
     ret = []
