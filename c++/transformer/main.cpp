9f216ecfb635aefee511ad4d1186c55aebbde402
wangli <freelw81@qq.com>
2025-03-30 20:19:17 +0800

    addnorm bp 精度 百万分之一

--- c++/transformer/main.cpp
+++ c++/transformer/main.cpp
@@ -680,12 +680,8 @@ int main() {
     // test_attention_to_cp_with_mha();
     // cout << "------ test_attention_to_cp_with_mha end ------" << endl;
     // test_lazy_liner();
-
-
     // test_mh_attention_with_mask();
-
     // test_pos_encoding();
-
     test_addnorm();
     return 0;
 }
