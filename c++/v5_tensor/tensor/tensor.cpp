968a76c3a0f53348bd58058a8c3729a5636e32d8
wangli <freelw81@qq.com>
2025-04-29 18:00:55 +0800

    update

--- c++/v5_tensor/tensor/tensor.cpp
+++ c++/v5_tensor/tensor/tensor.cpp
@@ -1,6 +1,6 @@
 #include "tensor.h"
 
-Tensor::Tensor(std::vector<int> _shape) : data(nullptr), shape(_shape) {
+Tensor::Tensor(std::vector<int> _shape) : shape(_shape), data(nullptr) {
     strides.resize(shape.size());
     strides[shape.size() - 1] = 1;
     for (int i = shape.size() - 2; i >= 0; --i) {
@@ -39,4 +39,38 @@ bool Tensor::sanitize() const {
         }
     }
     return true;
+}
+
+Tensor *Tensor::transpose_2d() {
+    if (shape.size() != 2) {
+        std::cerr << "Error: Transpose only supported for 2D tensors" << std::endl;
+        return nullptr;
+    }
+    if (data != nullptr) {
+        std::cerr << "Error: Transpose not supported for non-null data" << std::endl;
+        abort();
+    }
+    
+    Tensor *transposed_tensor = allocTensorView(this);
+    transposed_tensor->shape[0] = shape[1];
+    transposed_tensor->shape[1] = shape[0];
+    transposed_tensor->strides[0] = strides[1];
+    transposed_tensor->strides[1] = strides[0];
+    return transposed_tensor;
+}
+
+
+std::vector<Tensor*> g_tensors;
+std::vector<Tensor*> g_tensor_views;
+
+Tensor *allocTensor(const std::vector<int> &shape) {
+    Tensor *tensor = new Tensor(shape);
+    g_tensors.push_back(tensor);
+    return tensor;
+}
+
+Tensor *allocTensorView(Tensor *parent) {
+    Tensor *tensor_view = new TensorView(parent);
+    g_tensor_views.push_back(tensor_view);
+    return tensor_view;
 }
--- c++/v5_tensor/tensor/tensor.h
+++ c++/v5_tensor/tensor/tensor.h
@@ -8,31 +8,50 @@
 #define TENSOR_PADDING_SIZE 16
 
 class Tensor {
-public:
-    Tensor(std::vector<int> _shape);
-    ~Tensor();
-    virtual void set_data(void *ptr);
-    int size() const;
-    int capacity() const;
-    bool sanitize() const;
-    virtual bool is_view() const { return false; }
-private:
-    void *data;
-    std::vector<int> shape;
-    std::vector<int> strides;
+    public:
+        Tensor(std::vector<int> _shape);
+        ~Tensor();
+        virtual void set_data(void *ptr);
+        virtual void *get_data() const { return data; }
+        virtual int size() const;
+        virtual int capacity() const;
+        virtual bool sanitize() const;
+        virtual bool is_view() const { return false; }
+        std::vector<int> get_shape() const { return shape; }
+        Tensor *transpose_2d();
+    protected:
+        std::vector<int> shape;
+        std::vector<int> strides;
+    private:
+        void *data;
 };
 
 class TensorView : public Tensor {
-public:
-    TensorView(std::vector<int> _shape, Tensor *parent)
-        : Tensor(_shape), parent(parent) {}
-    bool is_view() const override { return true; }
-    void set_data(void *ptr) override {
-        std::cerr << "Error: Cannot set data for TensorView" << std::endl;
-        assert(false);
-    }
-private:
-    Tensor *parent;
+    public:
+        TensorView(Tensor *parent)
+            : Tensor(parent->get_shape()), parent(parent) {}
+        bool is_view() const override { return true; }
+        void set_data(void *ptr) override {
+            std::cerr << "Error: Cannot set data for TensorView" << std::endl;
+            assert(false);
+        }
+        void *get_data() const override {
+            return parent->get_data();
+        }
+        int size() const override {
+            return parent->size();
+        }
+        int capacity() const override {
+            return parent->capacity();
+        }
+        bool sanitize() const override {
+            return parent->sanitize();
+        }
+    private:
+        Tensor *parent;
 };
+
+Tensor *allocTensor(const std::vector<int> &shape);
+Tensor *allocTensorView(Tensor *parent);
 
 #endif
