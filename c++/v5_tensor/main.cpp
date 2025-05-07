0779feb9dc2380e262f99a982eaa5cce583e7de9
wangli <freelw81@qq.com>
2025-05-07 19:17:32 +0800

    train [wip]

--- c++/v5_tensor/main.cpp
+++ c++/v5_tensor/main.cpp
@@ -8,12 +8,52 @@
 #include "optimizers/adam.h"
 #include "model/mlp.h"
 
+#define INPUT_LAYER_SIZE 784
+
+void print_progress(uint i, uint tot) {
+    std::cout << "\r[" << i << "/" << tot << "]" << std::flush;
+}
+
+void assign_inputs(
+    Tensor *inputs, float *tmp_buffer,
+    int offset,
+    int batch_size,
+    const std::vector<std::vector<unsigned char>> &data) {
+    for (int i = 0; i < batch_size; ++i) {
+        for (int j = 0; j < INPUT_LAYER_SIZE; ++j) {
+            tmp_buffer[i * INPUT_LAYER_SIZE + j] = static_cast<float>(data[offset + i][j]) / 255.0f;
+        }
+    }
+    g_backend_ops->cp_to_device(
+        inputs,
+        reinterpret_cast<char*>(tmp_buffer),
+        inputs->size()
+    );
+}
+
+void assign_labels(
+    Tensor *labels, int32_t *tmp_buffer,
+    int offset,
+    int batch_size,
+    const std::vector<unsigned char> & train_labels) {
+
+    assert(labels->get_dtype() == INT32);
+    for (int i = 0; i < batch_size; ++i) {
+        tmp_buffer[i] = static_cast<int32_t>(train_labels[offset + i]);
+    }
+    g_backend_ops->cp_to_device(
+        labels,
+        reinterpret_cast<char*>(tmp_buffer),
+        labels->size()
+    );
+}
+
 void train(int epochs, float lr, int batch_size) {
     MnistLoaderBase loader;
     loader.load();
     std::cout << "data loaded." << std::endl;
 
-    MLP m(784, {30, 10});
+    MLP m(INPUT_LAYER_SIZE, {30, 10});
     Tensor *inputs = allocTensor({batch_size, 784}, "inputs");
     Tensor *labels = allocTensor({batch_size}, "labels", INT32);
     auto n_inputs = graph::allocNode(inputs);
@@ -26,16 +66,40 @@ void train(int epochs, float lr, int batch_size) {
     optimizer.step();
 
     allocMemAndInitTensors();
+    float *inputs_tmp_buffer = static_cast<float*>(::malloc(inputs->size()));
+    int32_t *labels_tmp_buffer = static_cast<int32_t*>(::malloc(labels->size()));
+    const std::vector<std::vector<unsigned char>> & train_images = loader.getTrainImages();
+    const std::vector<unsigned char> & train_labels = loader.getTrainLabels();
+    assert(train_images.size() % batch_size == 0);
     for (int epoch = 0; epoch < epochs; ++epoch) {
-        gDoActions();
-        std::cout << "loss : " << g_backend_ops->get_float(loss->get_tensor(), 0) << std::endl;
+        float loss_sum = 0;
+        int offset = 0;
+        int loop_times = 0;
+        print_progress(offset, train_images.size());
+        while (offset < train_images.size()) {
+            assign_inputs(
+                inputs,
+                static_cast<float*>(inputs_tmp_buffer),
+                offset,
+                batch_size,
+                train_images
+            );
+            offset += batch_size;
+            gDoActions();
+            loss_sum += g_backend_ops->get_float(loss->get_tensor(), 0);
+            loop_times++;
+            print_progress(offset, train_images.size());
+        }
+        std::cout << "loss : " << loss_sum / loop_times << std::endl;
     }
+    ::free(labels_tmp_buffer);
+    ::free(inputs_tmp_buffer);
 }
 
 int main(int argc, char *argv[]) {
     int opt;
     int epochs = 30;
-    int batch_size = 32;
+    int batch_size = 60;
     float lr = 0.001;
 
     while ((opt = getopt(argc, argv, "e:l:b:")) != -1) {
