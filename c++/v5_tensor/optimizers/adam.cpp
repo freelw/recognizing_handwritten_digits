fbd9b21e90887a2e314bf1dea99f54e8562507b5
wangli <freelw81@qq.com>
2025-05-07 10:24:39 +0800

    adam init

---
+++ c++/v5_tensor/optimizers/adam.cpp
@@ -0,0 +1,9 @@
+#include "adam.h"
+
+void Adam::step() {
+
+}
+
+bool Adam::clip_grad(float grad_clip_val) {
+    return false;
+}
---
+++ c++/v5_tensor/optimizers/adam.h
@@ -0,0 +1,23 @@
+#ifndef ADAM_H
+#define ADAM_H
+
+#include "graph/node.h"
+
+class Adam {
+    public:
+        Adam(std::vector<graph::Node*> _parameters, float _lr,
+            float _beta1 = 0.9, float _beta2 = 0.999, float _epsilon = 1e-8)
+            : parameters(_parameters), lr(_lr),
+                beta1(_beta1), beta2(_beta2), epsilon(_epsilon)
+            {}
+        void step();
+        bool clip_grad(float grad_clip_val);
+        
+    private:
+        std::vector<graph::Node*> parameters;
+        float lr; 
+        float beta1;
+        float beta2;
+        float epsilon;
+};
+#endif
