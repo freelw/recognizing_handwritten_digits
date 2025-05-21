badf1866a08daa6208fbbe19d9c639a396d704fe
wangli <freelw81@qq.com>
2025-05-21 15:49:11 +0800

    ce mask test

---
+++ c++/v5_tensor/pytorch_test/ce_mask_test.py
@@ -0,0 +1,34 @@
+import torch
+import torch.nn as nn
+import cmath
+from torch.nn import functional as F
+
+def test1():
+    
+    res = torch.tensor([[10, 0.2, 0.3], [0.4, 0.5, 0.6], [0.1, 0.2, 0.3]], dtype=torch.float32)
+    res.requires_grad_(True)
+    y = torch.tensor([1, 1, 2], dtype=torch.long)
+    loss = F.cross_entropy(res, y, reduction="none")
+    print("loss1: ", loss)
+    loss = loss.sum()/y.shape[0]
+    print("loss1 avg: ", loss)
+    loss.backward()
+
+def test2():
+    
+    res = torch.tensor([[10, 0.2, 0.3], [0.4, 0.5, 0.6], [0.1, 0.2, 0.3]], dtype=torch.float32)
+    res.requires_grad_(True)
+    y = torch.tensor([1, 1, 2], dtype=torch.long)
+    mask = (y.reshape(-1) != 2).type(torch.float32)
+    loss = F.cross_entropy(res, y, reduction="none")
+    print("loss2: ", loss)
+    print ("mask: ", mask)
+    print ("loss * mask: ", loss * mask)
+    loss_mask = (loss * mask).sum() / mask.sum()
+    print("loss_mask: ", loss_mask)
+    loss_mask.backward()   
+
+if __name__ == "__main__":
+    test1()
+    print("--------")
+    test2()
