e177a4e51a61e7932013213cb7748b20b0311cc6
wangli <freelw81@qq.com>
2025-05-24 11:10:46 +0800

    wip

--- c++/v5_tensor/transformer.cpp
+++ c++/v5_tensor/transformer.cpp
@@ -84,7 +84,9 @@ void load_tokens_from_file(
     std::vector<std::vector<uint>> &src_token_ids,
     std::vector<std::vector<uint>> &tgt_token_ids,
     int &enc_vocab_size,
-    int &dec_vocab_size
+    int &dec_vocab_size,
+    int &bos_id,
+    int &eos_id
     ) {
     std::string corpus = RESOURCE_NAME;
     std::string src_vocab_name = SRC_VOCAB_NAME;
@@ -94,6 +96,8 @@ void load_tokens_from_file(
     loader.get_token_ids(src_token_ids, tgt_token_ids);
     enc_vocab_size = loader.src_vocab_size();
     dec_vocab_size = loader.tgt_vocab_size();
+    bos_id = loader.tgt_bos_id();
+    eos_id = loader.tgt_eos_id();
 }
 
 int main(int argc, char *argv[]) {
@@ -126,16 +130,21 @@ int main(int argc, char *argv[]) {
 
     int enc_vocab_size = 0;
     int dec_vocab_size = 0;
+    int bos_id = 0;
+    int eos_id = 0;
 
     std::vector<std::vector<uint>> v_src_token_ids;
     std::vector<std::vector<uint>> v_tgt_token_ids;
     load_tokens_from_file(
         v_src_token_ids, v_tgt_token_ids,
-        enc_vocab_size, dec_vocab_size
+        enc_vocab_size, dec_vocab_size,
+        bos_id,
+        eos_id
     );
-
     std::cout << "enc_vocab_size : " << enc_vocab_size << std::endl;
     std::cout << "dec_vocab_size : " << dec_vocab_size << std::endl;
+    std::cout << "bos_id : " << bos_id << std::endl;
+    std::cout << "eos_id : " << eos_id << std::endl;
 
     use_gpu(gpu==1);
     construct_env();
@@ -145,8 +154,6 @@ int main(int argc, char *argv[]) {
     int ffn_num_hiddens = 64;
     int num_heads = 4;
     
-    int bos_id = 0; // fix me
-    int eos_id = 1; // fix me
     int num_steps = NUM_STEPS;
     int max_posencoding_len = MAX_POSENCODING_LEN;
     Seq2SeqEncoderDecoder *seq2seq = new Seq2SeqEncoderDecoder(
@@ -163,6 +170,31 @@ int main(int argc, char *argv[]) {
     Tensor *ce_mask = allocTensor({batch_size * num_steps}, INT32);
     auto ce_mask_node = graph::allocNode(ce_mask);
     ce_mask_node->init_weight_fill(1.0f);
+
+    // alloc input buffers
+    // 1. enc_valid_lens
+    // 2. src_token_ids
+    // 3. tgt_token_ids
+    // 4. labels
+    // 5. ce_mask
+    // 6. dec_valid_lens 在 init_dec_valid_lens 中申请，一次性构造
+
+    int32_t *enc_valid_lens_buffer = static_cast<int32_t *>(::malloc(
+        enc_valid_lens->size()
+    ));
+    int32_t *src_token_ids_buffer = static_cast<int32_t *>(::malloc(
+        src_token_ids->size()
+    ));
+    int32_t *tgt_token_ids_buffer = static_cast<int32_t *>(::malloc(
+        tgt_token_ids->size()
+    ));
+    int32_t *labels_buffer = static_cast<int32_t *>(::malloc(
+        labels->size()
+    ));
+    int32_t *ce_mask_buffer = static_cast<int32_t *>(::malloc(
+        ce_mask->size()
+    ));
+
     auto res = seq2seq->forward(src_token_ids, tgt_token_ids, enc_valid_lens, dec_valid_lens);
     auto loss = res->reshape({-1, dec_vocab_size})->CrossEntropy(labels)->mask(ce_mask)->avg_1d(ce_mask);
     insert_boundary_action();
@@ -178,14 +210,39 @@ int main(int argc, char *argv[]) {
     
     allocMemAndInitTensors();
     init_dec_valid_lens(dec_valid_lens);
-    for (int i = 0; i < epochs; ++i) {
-        for (int j = 0; j < 1306; ++j) {
-            std::string prefix = "epoch " + std::to_string(i) + " : ";
-            print_progress(prefix , j*batch_size, 1306*batch_size);
-            gDoActions();
+    for (int epoch = 0; epoch < epochs; ++epoch) {
+        float loss_sum = 0;
+        int cnt = 0;
+        std::string prefix = "epoch " + std::to_string(epoch) + " : ";
+        for (int i = 0; i < v_src_token_ids.size(); i += batch_size) {
+            cnt ++;
+
+            auto end = i + batch_size;
+            if (end > v_src_token_ids.size()) {
+                break;
+            }
+
+            for (int j = i; i < end; ++j) {
+
+            }
+
+            print_progress(prefix , end, v_src_token_ids.size());
+            
+            // for (int j = 0; j < 1306; ++j) {
+            //     std::string prefix = "epoch " + std::to_string(epoch) + " : ";
+            //     print_progress(prefix , j*batch_size, 1306*batch_size);
+            //     gDoActions();
+            // }
         }
         std::cout << "loss : " << *loss->get_tensor() << std::endl;
     }
+    
+    // free input buffers
+    ::free(enc_valid_lens_buffer);
+    ::free(src_token_ids_buffer);
+    ::free(tgt_token_ids_buffer);
+    ::free(labels_buffer);
+    ::free(ce_mask_buffer);
     
     delete seq2seq;
     destruct_env();
